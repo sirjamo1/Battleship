d92a4523b61ec0e6535062b6ea1517a821f85737
Sirjamo1 <jam_how@hotmail.com>
2022-12-31 10:57:40 +1000

    added ships cannot occupy same space, or be placed off the board

--- gameBoard.js
+++ gameBoard.js
@@ -13,18 +13,23 @@ class GameBoard {
         this.shipList = {
             carrier: {
                 length: 5,
+                quantity: 1,
             },
             battleship: {
                 length: 4,
+                quantity: 1,
             },
             cruiser: {
                 length: 3,
+                quantity: 1,
             },
             submarine: {
                 length: 3,
+                quantity: 1,
             },
             destroyer: {
                 length: 2,
+                quantity: 1,
             },
         };
 
@@ -40,23 +45,34 @@ class GameBoard {
             }
         }
     }
-    createShip(coord, dir, type) {
-        const ship = new Ship(this.shipList[type].length);
-        if (dir === "hor") {
-            for (let i = 0; i < ship.length; i += 1) {
-                ship.shipCoord.push([coord[0], coord[1] + i]);
-                this.board[coord[0]][coord[1] + i] = type;
-            }
-        } else if (dir === "ver") {
-            for (let i = 0; i < ship.length; i += 1) {
-                ship.shipCoord.push([coord[0] + i, coord[1]]);
-                this.board[coord[0] + i][coord[1]] = type;
-            }
-        }
+    createShip(coord, dir, shipName) {
+        const ship = new Ship(this.shipList[shipName].length);
+        let coordArray = this.getCoordArray(coord, dir, ship.length);
+        let largestShipLength = 5;
+        if (coordArray.length > largestShipLength) return coordArray;
+        ship.shipCoord = coordArray;
+        coordArray.forEach((square) => {
+            this.board[square[0]][square[1]] = shipName;
+        });
     }
     receiveAttack(coord) {
         this.board[coord[0]][coord[1]] = "x";
         return this.attackList.push(coord);
+    }
+    getCoordArray(coord, dir, shipLength) {
+        let coordArray = [];
+        for (let i = 0; i < shipLength; i += 1) {
+            let y = dir === "x" ? coord[0] : coord[0] + i;
+            let x = dir === "x" ? coord[1] + i : coord[1];
+            if (this.board[y][x].length !== 0) {
+                return `${this.board[y][x]} occupies this space`;
+            } else if (y > 9 && y < 0 && x > 9 && x < 0) {
+                return console.log("Ship must be placed on board");
+            } else {
+                coordArray.push([y, x]);
+            }
+        }
+        return coordArray;
     }
 }
 
--- gameBoard.test.js
+++ gameBoard.test.js
@@ -7,25 +7,35 @@ test("check squares", () => {
 expect(boardTest.board.length).toBe(10)
 })
 test("Adding ship (carrier)(horizontal), and check board for it", () => {
-    boardTest.createShip([1, 1], "hor", "carrier");
+    boardTest.createShip([1, 1], "x", "carrier");
     expect(boardTest.board[1][1] && boardTest.board[1][5]).toBe("carrier");
 });
+test("Try adding ship (destroyer) to same space as carrier", () => {
+    expect(boardTest.createShip([1, 3], 'x', "destroyer")).toBe("carrier occupies this space")
+})
+test("adding ship (cruiser) (vertical) and check board for it", () => {
+    boardTest.createShip([4, 4], "y", "cruiser");
+    expect(boardTest.board[4][4] && boardTest.board[6][4]).toBe("cruiser")
+})
 test("check either side carrier ship === []", () => {
- expect(boardTest.board[1][0] && boardTest.board[1][6]).toStrictEqual([])
+ expect(boardTest.board[1][0] && boardTest.board[1][6]).toStrictEqual([]);
 })
 
 test("check attackList is empty(before any attacks)", () => {
  expect(boardTest.attackList).toStrictEqual([]);
 })
 test("check attackList after receiveAttack is run", () => {
- boardTest.receiveAttack([1, 5])
- expect(boardTest.attackList).toStrictEqual([[1, 5]]);
+ boardTest.receiveAttack([3, 5])
+ expect(boardTest.attackList).toStrictEqual([[3, 5]]);
 });
 test("check attackList after receiveAttack is run again", () => {
     boardTest.receiveAttack([2, 6]);
-    expect(boardTest.attackList).toStrictEqual([[1, 5],[2, 6]]);
+    expect(boardTest.attackList).toStrictEqual([[3, 5],[2, 6]]);
 });
 test("check board to see if previous attacks has logged", () => {
     expect(boardTest.board[2][6]).toBe("x");
 });
 
+test("log board", () => {
+    console.log(boardTest.board)
+})
