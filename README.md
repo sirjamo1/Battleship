a15b20d2132b7beb6d736e8cdb3fc68e4fde6f29
Sirjamo1 <jam_how@hotmail.com>
2023-01-05 14:52:22 +1000

    added title, text log (for shots fired)

--- README.md
+++ README.md
@@ -10,4 +10,6 @@ sand Photo by Aleksandar Pasaric: https://www.pexels.com/photo/wavy-patterns-on-
 water Photo by Magda Ehlers: https://www.pexels.com/photo/body-of-water-636353/
 water 2 Photo by bt3gl ♡: https://www.pexels.com/photo/aerial-shot-of-blue-water-3894157/
 
+metal backgreound from https://pixabay.com/photos/surface-rivet-metal-facade-2754970/
+
 cursor from pngwing.com
--- src/index.js
+++ src/index.js
@@ -65,11 +65,14 @@ const fireShots = (x, y, player, square) => {
         playerTwo.playerGameBoard.board[x][y].slice(-3) === "HIT" ||
         playerTwo.playerGameBoard.board[x][y] === "x"
     )
-        return alert("Try another square");
+        return logToText("Try another square");
     //playerOne's shot
     playerTwo.playerGameBoard.receiveAttack([x, y]);
+    logToText(
+        `Player one attacked x:${x} y:${y} ${checkSquare(playerTwo, x, y)}`
+    );
     if (playerTwo.playerGameBoard.shipsLeft === 0)
-        return alert("Player One Won!");
+        return logToText("Player One Won!");
     player.playerGameBoard.board[x][y] === "x"
         ? (square.style.backgroundColor = "rgba(255, 255, 255, 0.6)")
         : ((square.style.backgroundColor = "rgba(255, 0, 0, 0.7)"),
@@ -84,6 +87,15 @@ const fireShots = (x, y, player, square) => {
             let heatSeekingCoord =
                 playerOne.playerGameBoard.heatSeekingList.shift();
             playerOne.playerGameBoard.receiveAttack(heatSeekingCoord);
+            logToText(
+                `Player Two attacked x:${heatSeekingCoord[0]} y:${
+                    heatSeekingCoord[1]
+                } ${checkSquare(
+                    playerOne,
+                    heatSeekingCoord[0],
+                    heatSeekingCoord[1]
+                )}`
+            );
             // if secondary heat seekers are available
         } else if (playerOne.playerGameBoard.secondaryHeatSeeker.length > 0) {
             playerOne.playerGameBoard.secondaryHeatSeeker =
@@ -93,16 +105,34 @@ const fireShots = (x, y, player, square) => {
                 playerOne.playerGameBoard.secondaryHeatSeeker.pop();
             // console.log(secondaryHeatSeeker, "sec heat");
             playerOne.playerGameBoard.receiveAttack(secondaryHeatSeeker);
+            logToText(
+                `Player Two attacked x:${secondaryHeatSeeker[0]} y:${
+                    secondaryHeatSeeker[1]
+                } ${checkSquare(
+                    playerOne,
+                    secondaryHeatSeeker[0],
+                    secondaryHeatSeeker[1]
+                )}`
+            );
+
             // take a random shot
         } else {
             playerOne.randomShot();
+            console.log(playerOne);
+            let list = playerOne.playerGameBoard.attackList;
+            let index = list.length - 1;
+            checkSquare(playerOne, list[index][0], list[index][1]);
+            logToText(
+                `Player Two attacked x:${list[index][0]} y:${
+                    list[index][1]
+                } ${checkSquare(playerOne, list[index][0], list[index][1])}`
+            );
         }
         // get where shot hit
         let playerTwoShot =
             playerOne.playerGameBoard.attackList[
                 playerOne.playerGameBoard.attackList.length - 1
             ];
-        console.log(playerTwoShot, "<-- player twos shot");
         // get corresponding div
         let playerOneSquare = document.getElementById(
             `player-one-${playerTwoShot[0]}${playerTwoShot[1]}`
@@ -110,12 +140,17 @@ const fireShots = (x, y, player, square) => {
         // change div background color
         playerOne.playerGameBoard.board[playerTwoShot[0]][playerTwoShot[1]] ===
         "x"
-            ? (playerOneSquare.style.backgroundColor = "rgba(255, 255, 255, 0.6)")
-            : (playerOneSquare.style.backgroundColor =
-                  "rgba(255, 0, 0, 0.7)");
+            ? (playerOneSquare.style.backgroundColor =
+                  "rgba(255, 255, 255, 0.6)")
+            : (playerOneSquare.style.backgroundColor = "rgba(255, 0, 0, 0.7)");
         if (playerOne.playerGameBoard.shipsLeft === 0)
             return alert("Player Two Won!");
-    }, 1000);
+    }, 1300);
+};
+const checkSquare = (player, x, y) => {
+    console.log(player.playerGameBoard.board[x][y]);
+    if (player.playerGameBoard.board[x][y] === "x") return "MISS";
+    if (player.playerGameBoard.board[x][y].slice(-3) === "HIT") return "HIT!";
 };
 const createShipDiv = (shipName, shipImageName) => {
     const shipImage = new Image();
@@ -124,7 +159,9 @@ const createShipDiv = (shipName, shipImageName) => {
     shipImage.draggable = true;
     shipImage.ondragstart = (e) => {
         shipSelection = shipName;
-        drag(e);
+        //  shipImage.style.position = 'absolute'
+        // let shiftX = e.clientX
+        drag(e, shipImage);
     };
 
     shipImage.id = `${shipName}-option`;
@@ -137,7 +174,10 @@ const createShipDiv = (shipName, shipImageName) => {
 const allowDrop = (e) => {
     e.preventDefault();
 };
-const drag = (e) => {
+const drag = (e, shipImage) => {
+    // shipImage.style.left = "0px"
+    // shipImage.style.top = "0px";
+
     e.dataTransfer.setData("img", e.target.id);
 };
 const drop = (e, x, y) => {
@@ -149,6 +189,9 @@ const drop = (e, x, y) => {
     );
     putShipOnboard(playerOne);
     removeShipOption(shipSelection);
+};
+const logToText = (message) => {
+    document.getElementById("log-text").innerHTML = message;
 };
 const shipPlacement = () => {
     const placementContainer = document.createElement("div");
@@ -174,41 +217,49 @@ const shipPlacement = () => {
     randomPlacement.onclick = () => {
         playerOne.populateBoard();
         putShipOnboard(playerOne);
-        placementContainer.removeChild(
-            document.getElementById("carrier-option")
-        );
-        placementContainer.removeChild(
-            document.getElementById("battleship-option")
-        );
-        placementContainer.removeChild(
-            document.getElementById("cruiser-option")
-        );
-        placementContainer.removeChild(
-            document.getElementById("submarine-option")
-        );
+        removeShipOption("carrier");
+        removeShipOption("battleship");
+        removeShipOption("cruiser");
+        removeShipOption("submarine");
         removeShipOption("destroyer");
     };
+    const shipContainer = document.createElement("div");
+    shipContainer.id = "ship-container";
+
     placementContainer.appendChild(randomPlacement);
     placementContainer.appendChild(directionButton);
-    placementContainer.appendChild(createShipDiv("carrier", carrier));
-    placementContainer.appendChild(createShipDiv("battleship", battleship));
-    placementContainer.appendChild(createShipDiv("cruiser", cruiser));
-    placementContainer.appendChild(createShipDiv("submarine", submarine));
-    placementContainer.appendChild(createShipDiv("destroyer", destroyer));
+    placementContainer.appendChild(shipContainer);
+    shipContainer.appendChild(createShipDiv("carrier", carrier));
+    shipContainer.appendChild(createShipDiv("battleship", battleship));
+    shipContainer.appendChild(createShipDiv("destroyer", destroyer));
+    shipContainer.appendChild(createShipDiv("cruiser", cruiser));
+    shipContainer.appendChild(createShipDiv("submarine", submarine));
+
     return placementContainer;
+};
+const createTitle = () => {
+    const title = document.createElement("h1");
+    title.id = "title";
+    title.innerHTML = "Battleships";
+    return title
 };
 function component() {
     const mainContainer = document.createElement("div");
+    
     mainContainer.id = "main-container";
+    mainContainer.appendChild(createTitle());
     const playerOneContainer = document.createElement("div");
     playerOneContainer.id = "player-one-container";
     playerOneContainer.appendChild(createPlayerBoard("player-one", playerOne));
-    playerOneContainer.appendChild(shipPlacement());
+    // playerOneContainer.appendChild(shipPlacement());
+
     mainContainer.appendChild(playerOneContainer);
     const playerTwoContainer = document.createElement("div");
     playerTwoContainer.id = "player-two-container";
     playerTwoContainer.appendChild(createPlayerBoard("player-two", playerTwo));
+
     mainContainer.appendChild(playerTwoContainer);
+    mainContainer.appendChild(shipPlacement());
 
     return mainContainer;
 }
@@ -226,24 +277,25 @@ const putShipOnboard = (player) => {
             const imageDiv = document.createElement("div");
             imageDiv.classList.add(`${shipName}-sprite-${num}`);
             if (square.childNodes.length === 0) {
-            square.appendChild(imageDiv);
-            num += 1;
-            if (player.className === "player-two") {
-                imageDiv.style.opacity = "0";
+                square.appendChild(imageDiv);
+                num += 1;
+                if (player.className === "player-two") {
+                    imageDiv.style.opacity = "0";
+                }
+                if (dir === "y") {
+                    imageDiv.style.transform = "rotate(90deg)";
+                }
             }
-            if (dir === "y") {
-                imageDiv.style.transform = "rotate(90deg)";
-            }
-        }
         });
     }
 };
 const resetGame = () => {
-    playerOne = new Player();
-    playerTwo = new Player();
+    playerOne = new Player("player-one");
+    playerTwo = new Player("player-two");
     gameStarted = false;
     shipSelection = null;
     shipPlacementDirection = "x";
+    let mainContainer = document.getElementById("main-container");
     let playerOneContainer = document.getElementById("player-one-container");
     let playerTwoContainer = document.getElementById("player-two-container");
     let shipPlacementContainer = document.getElementById(
@@ -259,12 +311,27 @@ const resetGame = () => {
         createPlayerBoard("player-two", playerTwo),
         playerTwoBoard
     );
-    playerOneContainer.replaceChild(shipPlacement(), shipPlacementContainer);
+    mainContainer.replaceChild(shipPlacement(), shipPlacementContainer);
+};
+const CreateMoveLogContainer = () => {
+    const logTextContainer = document.createElement("div");
+    logTextContainer.id = "log-text-container";
+    const logText = document.createElement("h3");
+    logText.id = "log-text";
+    logText.innerHTML = "Click Start to Play";
+    logTextContainer.appendChild(logText);
+    return logTextContainer;
 };
 const removeShipOption = (ship) => {
-    let container = document.getElementById("ship-placement-container");
+    let mainContainer = document.getElementById("main-container");
+    let shipPlacementContainer = document.getElementById(
+        "ship-placement-container"
+    );
+    let shipContainer = document.getElementById("ship-container");
     let element = document.getElementById(`${ship}-option`);
-    container.removeChild(element);
+    if (element.parentElement === shipContainer) {
+        shipContainer.removeChild(element);
+    }
     if (playerOne.playerGameBoard.shipsNotDeployed <= 0) {
         const resetButton = document.createElement("button");
         resetButton.id = "reset-button";
@@ -277,16 +344,18 @@ const removeShipOption = (ship) => {
         startButton.innerHTML = "Start";
         startButton.onclick = () => {
             gameStarted = true;
-            container.removeChild(startButton);
+            mainContainer.removeChild(startButton);
             playerTwo.populateBoard();
             putShipOnboard(playerTwo);
             disablePlayerOneBoardClick();
         };
-        container.replaceChild(
-            startButton,
-            document.getElementById("direction-button")
+        mainContainer.appendChild(startButton);
+        shipPlacementContainer.replaceChild(
+            CreateMoveLogContainer(),
+            shipContainer
         );
-        container.replaceChild(
+
+        shipPlacementContainer.replaceChild(
             resetButton,
             document.getElementById("random-placement-button")
         );
